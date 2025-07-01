b464d647321a0c540527d0b3d0213b4044fdc215
RunThem <iccy.fun@outlook.com>
2025-07-01 14:13:18 +0800

    feat(dep): third party dependencies become private

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@ pub mod traits;
 
 pub use crate::traits::*;
 
-pub extern crate bytes;
-pub extern crate itertools;
+extern crate bytes;
+extern crate itertools;
 
 pub use bytes::{Bytes, BytesMut};
 pub use itertools::Itertools;
