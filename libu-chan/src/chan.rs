e40a696d8d7cef3a541e06e6c6d57a539af0228c
RunThem <iccy.fun@outlook.com>
2026-05-28 14:33:02 +0800

    feat(chan): replace std::sync::mpsc with flume and add AsRef impls

--- libu-chan/src/chan.rs
+++ libu-chan/src/chan.rs
@@ -1,23 +1,43 @@
-use std::sync::mpsc;
+use flume::{Receiver, RecvError, SendError, Sender, TryRecvError, unbounded};
 
 /// 点对点线程安全的双向消息队列
-pub struct Chan<S, R>(mpsc::Sender<S>, mpsc::Receiver<R>);
+pub struct Chan<S, R>(Sender<S>, Receiver<R>);
 
 impl<S, R> Chan<S, R> {
-  pub fn send(&self, msg: S) -> Result<(), mpsc::SendError<S>> {
+  pub fn send(&self, msg: S) -> Result<(), SendError<S>> {
     self.0.send(msg)
   }
 
-  pub fn recv(&self) -> Result<R, mpsc::RecvError> {
+  pub fn recv(&self) -> Result<R, RecvError> {
     self.1.recv()
   }
 
-  pub fn try_recv(&self) -> Result<R, mpsc::TryRecvError> {
+  pub fn try_recv(&self) -> Result<R, TryRecvError> {
     self.1.try_recv()
   }
 
   pub fn iter(&self) -> impl Iterator<Item = R> + '_ {
     std::iter::from_fn(|| self.recv().ok())
+  }
+
+  pub fn tx(&self) -> &Sender<S> {
+    &self.0
+  }
+
+  pub fn rx(&self) -> &Receiver<R> {
+    &self.1
+  }
+}
+
+impl<S, R> AsRef<Sender<S>> for Chan<S, R> {
+  fn as_ref(&self) -> &Sender<S> {
+    &self.0
+  }
+}
+
+impl<S, R> AsRef<Receiver<R>> for Chan<S, R> {
+  fn as_ref(&self) -> &Receiver<R> {
+    &self.1
   }
 }
 
@@ -28,8 +48,8 @@ impl<S, R> std::fmt::Debug for Chan<S, R> {
 }
 
 pub fn channel<S, R>() -> (Chan<S, R>, Chan<R, S>) {
-  let (t0, r0) = mpsc::channel::<S>();
-  let (t1, r1) = mpsc::channel::<R>();
+  let (t0, r0) = unbounded::<S>();
+  let (t1, r1) = unbounded::<R>();
 
   (Chan(t0, r1), Chan(t1, r0))
 }
